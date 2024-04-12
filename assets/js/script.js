e9ad60f023a94036e7fe01b03fb96f671db2592b
Matthew <trueblood.mt@gmail.com>
2024-04-12 18:31:05 -0500

    stored card data to local storage and created a array of cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
@@ -9,7 +9,20 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+const taskArray = []
+task.forEach((cardEl)=>{ 
+const taskCard = `
+<div class="taskCard">
+<h3>${cardEl.title}</h3>
+<p>${cardEl.date}</p>
+<p>${cardEl.description}</p>
+</div>
 
+`
+const taskItem = $(taskCard)
+taskArray.push(taskItem)
+})
+return taskArray
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -19,6 +32,21 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+event.preventDefault()
+var taskNameEl = $("#taskName").val()
+var taskDateEl = $("#taskDate").val()
+var taskDescriptionEl = $("#taskDescription").val()
+console.log(taskNameEl,taskDateEl,taskDescriptionEl)
+var newTask={
+    title:taskNameEl,
+    date:taskDateEl,
+    description:taskDescriptionEl
+}
+taskList.push(newTask)
+localStorage.setItem("tasks",JSON.stringify(taskList))
+$("#formModal").modal("hide")
+createTaskCard(taskList)
+renderTaskList()
 
 }
 
@@ -34,5 +62,8 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    $("#addTaskBtn").on("click",handleAddTask)
+// creat event listener for my modal input button 
+// have it call handle add task
+// try to console log the values of the inputs from the modal 
 });
